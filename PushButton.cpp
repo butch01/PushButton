19894564fdb377671ffbb57bbca3809ba1034601
butch01 <as.butcher@gmx.de>
2017-11-06 21:34:45 +0100

    initial upload

---
+++ PushButton.cpp
@@ -0,0 +1,202 @@
+/*
+ * PushButton.cpp
+ *
+ *  Created on: 22.04.2016
+ *      Author: butch
+ */
+
+#include "PushButton.h"
+#include "arduino.h"
+
+#define BUTTON_STATE_PRESSED false
+#define BUTTON_STATE_DEFAULT true
+
+#define IS_DEBUG_PUSHBUTTON 0
+
+
+/**
+ * mode 0 for internal /use GPIO),
+ * mode 1 for external (set status directly by other source).
+ * buttonPin will be set to -1 if mode is external is optional and will be ignored.
+ *
+ */
+PushButton::PushButton(char mode, int buttonPin = -1) {
+	// check the given mode
+	switch (mode)
+	{
+		// we are using pin input
+		_mode = mode;
+		_buttonPin = buttonPin;
+		pinMode(_buttonPin, INPUT_PULLUP);
+
+
+
+	}
+
+	_buttonPin = buttonPin;
+	pinMode(_buttonPin, INPUT_PULLUP);
+
+	_debounceTimeMs=30;
+	_logicalStateMinTimeButtonPressed=50;
+	_lastButtonStateChangedTime=millis();
+	_hasLogicalStateChangeBeenProcessed=false;
+	_buttonState=BUTTON_STATE_DEFAULT;
+	_buttonStateUnbounced=BUTTON_STATE_DEFAULT;
+	_logicalState=false;
+	_debounceTimeStart=-1;
+	processButtonState();
+}
+
+PushButton::~PushButton() {
+	// TODO Auto-generated destructor stub
+}
+
+/**
+ * sets the debounce time
+ */
+void PushButton::setDebounceTimeMs(int time)
+{
+	_debounceTimeMs=time;
+}
+
+
+void PushButton::processButtonState()
+{
+	// get current button state ti have consistent baseline for this run
+	bool currentButtonState = digitalRead(_buttonPin);
+	long currentTime = millis();
+	
+	#if IS_DEBUG_PUSHBUTTON
+		Serial.print("currentButtonState=");
+		Serial.print(currentButtonState);
+		Serial.print(" currentTime=");
+		Serial.print(currentTime);
+		Serial.print(" ");
+	#endif
+
+	// check if debounce time is not started
+	if (_debounceTimeStart == -1)
+	{
+		// debounce Time is not started
+
+		// check if button is pressed
+		if (currentButtonState == BUTTON_STATE_PRESSED)
+		{
+			// start debounce time
+			_debounceTimeStart = currentTime;
+
+			// mark logical button state change as outstanding
+			_hasLogicalStateChangeBeenProcessed=false;
+		}
+		else
+		{
+			// debounce intervall has not been started && button is not pressed
+					//-> do nothing
+		}
+
+	}
+	else
+	{
+		// debounceTimeStart has been started
+		// check if we are in debounce interval
+		if (currentTime - _debounceTimeStart < _debounceTimeMs)
+		{
+			// since we are inside the debounce interval, ignore switches of the button state and do nothing
+			// -> do nothing
+			
+		}
+		else
+		{
+			// we are outside of debounce time interval / debounce time interval is through
+			// check if button is pressed
+			if (currentButtonState == BUTTON_STATE_PRESSED)
+			{
+				// debounce time has been started && and debounce time is through && button is pressed
+
+				// check if logical button state change already has been applied
+				if (_hasLogicalStateChangeBeenProcessed)
+				{
+					// has been applied
+					//-> nothing to do
+				}
+				else
+				{
+					// has not been applied yet
+					// flip the logical state
+					if (_logicalState)
+					{
+						// true -> flip to false
+						_logicalState=false;
+					}
+					else
+					{
+						// false -> flip to true
+						_logicalState=true;
+					}
+
+					// remember that logical state change has been done
+					_hasLogicalStateChangeBeenProcessed=true;
+				}
+			}
+			else
+			{
+				// button is not pressed / time is through
+				// -> reset the timers, etc.
+				_hasLogicalStateChangeBeenProcessed = false;
+				_debounceTimeStart=-1;
+
+
+			}
+			_buttonStateUnbounced = currentButtonState;
+		}
+	}
+
+	#if IS_DEBUG_PUSHBUTTON
+		Serial.print("_debounceTimeStart=");
+		Serial.print(_debounceTimeStart);
+		Serial.print(" _debounceTimeMs=");
+		Serial.print(_debounceTimeMs);
+		Serial.print(" _hasLogicalStateChangeBeenProcessed=");
+		Serial.print(_hasLogicalStateChangeBeenProcessed);
+		Serial.print(" _buttonState=");
+		Serial.print(_buttonState);
+		Serial.print(" _logicalState=");
+		Serial.print(_logicalState);
+		Serial.print("\n");
+	#endif
+
+}
+
+/**
+ * returns the unbounced button state
+ */
+bool PushButton::getButtonStateRawDebounced()
+{
+	return _buttonStateUnbounced;
+}
+
+/**
+ * returns the real state of the button (pressed / not pressed)
+ */
+bool PushButton::getButtonStateRawData()
+{
+	return _buttonState;
+}
+
+/**
+ * returns the logical state of the button which flips between true and false by each press
+ */
+bool PushButton::getButtonStateLogical()
+{
+	return _logicalState;
+}
+
+
+/**
+ * return - time in millis since the last raw button state has been changed.
+ *          In other words: How long has the button been pressed / not pressed since last action on button
+ */
+long PushButton::getTimeOfCurrentRawButtonState()
+{
+	return millis()- _lastButtonStateChangedTime;
+}
---
+++ PushButton.h
@@ -0,0 +1,50 @@
+/*
+ * PushButton.h
+ *
+ *  Created on: 22.04.2016
+ *      Author: butch
+ */
+
+#ifndef PUSHBUTTON_H_
+#define PUSHBUTTON_H_
+
+/**
+ * this class is a push button implementation. it will use INPUT_PULLUP as pinMode for the given pin.
+ * It will debounce the (un)pressing of the button. Only if state stays for longer than the debounceTimeMs it will switch the state.
+ *
+ * real state will give debounced value if button is pressed or not
+ * logical state is the logical on / off status which will be flipped by each button press. It will flip after debounce time.
+ *   If the button stays pressed it will flip only once. For further flip the button needs to be relased and pressed again.
+ */
+class PushButton {
+private:
+	int _buttonPin;
+	int _debounceTimeMs; // default 100ms // if state consists longer than that time, the state will switch
+	long _debounceTimeStart;
+	
+	bool _buttonState; // real state of button (pressed / not pressed). Debounce filter has been applied.
+	bool _buttonStateUnbounced;
+	long _lastButtonStateChangedTime; // time of last state change of real button press
+	bool _hasLogicalStateChangeBeenProcessed; // internal bool variable. Will be reset to false if button press is released.
+	
+
+	bool _logicalState; // Switch button will flip a logical state like a real on / off switch
+	long _logicalStateMinTimeButtonPressed; // button needs to be pressed at least for this time to recognize the press and initiate the logical state change
+
+	char _mode; // mode which will be used. (0: use pin input, 1: use alternate input -> set state from outside)
+
+
+public:
+	PushButton(char _mode, int _buttonPin); // mode 0 for internal /use GPIO), mode 1 for external (set status directly by other source). buttonPin will be set to -1 if mode is external is optional and will be ignored.
+	void setPin();
+	virtual ~PushButton();
+	void setDebounceTimeMs(int time);
+	void processButtonState();
+	bool getButtonStateRawData();
+	bool getButtonStateRawDebounced();
+	bool getButtonStateLogical();
+	long getTimeOfCurrentRawButtonState(); // how long is the current raw button state existing?
+
+};
+
+#endif /* PUSHBUTTON_H_ */
